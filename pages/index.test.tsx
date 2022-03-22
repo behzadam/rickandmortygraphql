07c094c0ac28fd631197352a6f696bd34899daa7
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-03-22 23:53:46 +0430

    test: add home page test

--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -1,18 +1,12 @@
 import React from 'react'
 
-import { render, screen } from '../utils/test-utils'
+import { render } from '../utils/test-utils'
 import Home from './index'
 
 describe('HomePage', () => {
   it('should render the heading', () => {
-    render(<Home />)
-
-    const heading = screen.getByRole('heading', {
-      name: /Rick and Morty | React Query Nextjs Graphql!/i,
-    })
-
-    // we can only use toBeInTheDocument because it was imported
-    // in the jest.setup.js and configured in jest.config.js
-    expect(heading).toBeInTheDocument()
+    const { asFragment } = render(<Home />)
+    const firstRender = asFragment()
+    expect(firstRender).toMatchSnapshot(asFragment())
   })
 })
